21f2267007cc32da1d63b84df856c5d64bf45217
s2108253 <s2108253@gmail.com>
2011-06-25 12:59:39 +0000

    Performed a general tidy up. One wire switch functions moved to a seperate code file. Metro Arduino library now used for a time scheduler.

---
+++ Supported/AT90CAN128/OneWireSwitches.cpp
@@ -0,0 +1,72 @@
+#include "OneWireSwitches.h"
+#include "WProgram.h"
+#include "Configuration.h"
+
+/*
+void InitialiseOneWireSwitches()
+{
+   Serial1.begin(330); 	//Odd value because the ATTINY uses internal clock which has a lot of error. 
+							//Calibration of ATTINY required.
+
+  newSwitchState = 7;
+  oldSwitchState = 0;  
+
+  pinMode(HORN_OUTPUT_PIN, OUTPUT);
+
+}
+
+void readSwitches()
+{
+  oldSwitchState = newSwitchState;
+		
+  //Add checking / timeout here, currently if switches not connected it locks the software.
+  newSwitchState = Serial1.read(); 
+		
+  //To fix - somtimes does not read the switches properly:
+  if(newSwitchState == 255)
+    newSwitchState = oldSwitchState;
+  
+  Serial1.flush();
+		
+
+  if(newSwitchState != oldSwitchState)
+  {
+    switch(newSwitchState)
+    {
+      case SWITCH_UP:
+          Menu++;
+          if(Menu > 3)
+            Menu = 0;
+          
+	  digitalWrite(HORN_OUTPUT_PIN, 0);
+          digitalWrite(DISPLAY_RESET_PIN, 0);
+      break;
+    
+      case SWITCH_DOWN:
+      if(Menu == 0)
+        Menu = 3;
+      else
+        Menu--;
+      
+      digitalWrite(HORN_OUTPUT_PIN, 0);
+      digitalWrite(DISPLAY_RESET_PIN, 0);
+      break;
+
+      case SWITCH_HORN:
+      digitalWrite(HORN_OUTPUT_PIN, 1);
+      digitalWrite(DISPLAY_RESET_PIN, 0);
+      break;
+      
+      case ALL_SWITCH_PRESSED:
+      digitalWrite(DISPLAY_RESET_PIN, 1);
+      digitalWrite(HORN_OUTPUT_PIN, 0);
+      break;
+      
+      default:
+      digitalWrite(HORN_OUTPUT_PIN, 0);
+      digitalWrite(DISPLAY_RESET_PIN, 0);
+    }
+  }
+}
+
+*/
---
+++ Supported/AT90CAN128/OneWireSwitches.h
@@ -0,0 +1,13 @@
+#define SWITCH_UP 6
+#define SWITCH_DOWN  3
+#define SWITCH_HORN 5
+#define ALL_SWITCH_PRESSED 0
+
+#define DISPLAY_RESET_PIN 39
+
+char oldSwitchState;
+char newSwitchState;
+
+void InitialiseOneWireSwitches();
+void readSwitches();
+
