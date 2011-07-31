4edee300af51267134c6c5bd41d38a34d3ab831e
s2108253@gmail.com <s2108253@gmail.com@2bc8ab89-cfdc-56b2-0442-f74da7ee8f6d>
2011-07-31 21:08:48 +0000

    Converted all code to use SGC.
    All graphics on the OLED display are now drawn using direct commands from the ATMEL controller board.
    
    The OLED display must be reflashed with the respecive SGC firmware

---
+++ trunk/Supported/AT90CAN128/fourdglFunctions.h
@@ -0,0 +1,17 @@
+#include <uOLED.h>              
+#include <colors.h>
+#include <float.h>
+#include <stdlib.h>
+
+#define GAUGE_CENTRE_X 63
+#define GAUGE_CENTRE_Y 63
+#define GAUGE_NEEDLE_LENGTH 55
+#define GAUGE_OUTER_CIRCLE_DIAMETER 63
+#define GAUGE_TICK_LENGTH 5
+
+extern uOLED uoled;                    // create an instance of the uOLED class
+
+void fourdglFunctionsInit();
+void drawGaugeBackground();
+void DrawPointer(int data, int maxValue);
+void intDebug(int data, int row);
